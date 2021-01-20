c7b9fa3a097e1f69971fa8d309cf669f3ad3a153
mei23 <m@m544.net>
2021-01-21 08:30:12 +0900

    test

--- test/ap-route.ts
+++ test/ap-route.ts
@@ -12,7 +12,7 @@ process.env.NODE_ENV = 'test';
 
 import * as childProcess from 'child_process';
 import * as assert from 'assert';
-import { async, signup, simpleGet, launchServer } from './utils';
+import { async, signup, post, simpleGet, launchServer } from './utils';
 
 // Request Accept
 const ONLY_AP = 'application/activity+json';
@@ -24,13 +24,17 @@ const UNSPECIFIED = '*/*';
 const AP = 'application/activity+json; charset=utf-8';
 const HTML = 'text/html; charset=utf-8';
 
-describe('ap-route', () => {
+describe('AP route negotiation', () => {
 	let p: childProcess.ChildProcess;
 
 	let alice: any;
+	let alicesPost: any;
 
 	before(launchServer(g => p = g, async () => {
 		alice = await signup({ username: 'alice' });
+		alicesPost = await post(alice, {
+			text: 'test'
+		});
 	}));
 
 	after(() => {
@@ -86,6 +90,32 @@ describe('ap-route', () => {
 			const res = await simpleGet(`/users/${alice.id}`, UNSPECIFIED);
 			assert.strictEqual(res.status, 302);
 			assert.strictEqual(res.location, `/@${alice.username}`);
+		}));
+	});
+
+	describe('/notes/:id', () => {
+		it('Only AP => AP', async(async () => {
+			const res = await simpleGet(`/notes/${alicesPost.id}`, ONLY_AP);
+			assert.strictEqual(res.status, 200);
+			assert.strictEqual(res.type, AP);
+		}));
+
+		it('Prefer AP => AP', async(async () => {
+			const res = await simpleGet(`/notes/${alicesPost.id}`, PREFER_AP);
+			assert.strictEqual(res.status, 200);
+			assert.strictEqual(res.type, AP);
+		}));
+
+		it('Prefer HTML => HTML', async(async () => {
+			const res = await simpleGet(`/notes/${alicesPost.id}`, PREFER_HTML);
+			assert.strictEqual(res.status, 200);
+			assert.strictEqual(res.type, HTML);
+		}));
+
+		it('Unspecified => HTML', async(async () => {
+			const res = await simpleGet(`/notes/${alicesPost.id}`, UNSPECIFIED);
+			assert.strictEqual(res.status, 200);
+			assert.strictEqual(res.type, HTML);
 		}));
 	});
 });
