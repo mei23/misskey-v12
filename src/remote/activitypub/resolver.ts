4e803a7ddddfb70dc7b66b9fd171ac58c338e699
mei23 <m@m544.net>
2021-06-04 23:12:18 +0900

    n

--- src/remote/activitypub/resolver.ts
+++ src/remote/activitypub/resolver.ts
@@ -63,3 +63,36 @@ export default class Resolver {
 		return object;
 	}
 }
+
+type MockResponse = {
+	type: string;
+	content: string;
+};
+
+class MockResolver extends Resolver {
+	private _rs = new Map<string, MockResponse>();
+	public async _register(uri: string, content: string | Record<string, any>, type = 'application/activity+json') {
+		this._rs.set(uri, {
+			type,
+			content: typeof content === 'string' ? content : JSON.stringify(content)
+		});
+	}
+
+	public async resolve(value: string | IObject): Promise<IObject> {
+		if (typeof value !== 'string') return value;
+
+		const r = this._rs.get(value);
+
+		if (!r) {
+			throw {
+				name: `StatusError`,
+				statusCode: 404,
+				message: `Not registed for mock`
+			};
+		}
+
+		const object = JSON.parse(r.content);
+
+		return object;
+	}
+}
--- test/activitypub.ts
+++ test/activitypub.ts
@@ -17,40 +17,7 @@ import Resolver from '../src/remote/activitypub/resolver';
 import { launchServer, signup, post, request, simpleGet, port, shutdownServer } from './utils';
 
 //#region Mock
-type MockResponse = {
-	type: string;
-	content: string;
-};
 
-class MockResolver extends Resolver {
-	/*
-	private _rs = new Map<string, MockResponse>();
-	public async _register(uri: string, content: string | Record<string, any>, type = 'application/activity+json') {
-		this._rs.set(uri, {
-			type,
-			content: typeof content === 'string' ? content : JSON.stringify(content)
-		});
-	}
-
-	public async resolve(value: string | IObject): Promise<IObject> {
-		if (typeof value !== 'string') return value;
-
-		const r = this._rs.get(value);
-
-		if (!r) {
-			throw {
-				name: `StatusError`,
-				statusCode: 404,
-				message: `Not registed for mock`
-			};
-		}
-
-		const object = JSON.parse(r.content);
-
-		return object;
-	}
-	*/
-}
 //#endregion
 describe('ActivityPub', () => {
 	let p: childProcess.ChildProcess;
