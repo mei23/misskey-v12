527cb0622fbfe8d298c7b3928feae01968633464
mei23 <m@m544.net>
2021-06-05 18:09:01 +0900

    nl

--- test/activitypub.ts
+++ test/activitypub.ts
@@ -7,7 +7,6 @@
  * To specify test:
  * > npx cross-env TS_NODE_FILES=true TS_NODE_TRANSPILE_ONLY=true TS_NODE_PROJECT="./test/tsconfig.json" npx mocha test/activitypub.ts --require ts-node/register -g 'test name'
  */
-
 process.env.NODE_ENV = 'test';
 
 import rndstr from 'rndstr';
