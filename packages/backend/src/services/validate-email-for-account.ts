6c837b9d6e09258003925f2a0ace58fd2732d683
mei23 <m@m544.net>
2021-11-25 00:27:31 +0900

    Remove email validation

--- packages/backend/src/services/validate-email-for-account.ts
+++ packages/backend/src/services/validate-email-for-account.ts
@@ -1,4 +1,3 @@
-import validateEmail from 'deep-email-validator';
 import { UserProfiles } from '@/models';
 
 export async function validateEmailForAccount(emailAddress: string): Promise<{
@@ -10,25 +9,12 @@ export async function validateEmailForAccount(emailAddress: string): Promise<{
 		email: emailAddress,
 	});
 
-	const validated = await validateEmail({
-		email: emailAddress,
-		validateRegex: true,
-		validateMx: true,
-		validateTypo: false, // TLDを見ているみたいだけどclubとか弾かれるので
-		validateDisposable: true, // 捨てアドかどうかチェック
-		validateSMTP: false, // 日本だと25ポートが殆どのプロバイダーで塞がれていてタイムアウトになるので
-	});
-
-	const available = exist === 0 && validated.valid;
+	const available = exist === 0;
 
 	return {
 		available,
 		reason: available ? null :
 			exist !== 0 ? 'used' :
-			validated.reason === 'regex' ? 'format' :
-			validated.reason === 'disposable' ? 'disposable' :
-			validated.reason === 'mx' ? 'mx' :
-			validated.reason === 'smtp' ? 'smtp' :
 			null,
 	};
 }
